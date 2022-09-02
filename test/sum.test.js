73edcd54ba2c482cc098d4a62d7aafcec426711e
initial@gmail.com <teamdailypractice@gmail.com>
2022-09-02 09:04:22 +0800

    sum - unit tests

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,17 @@
-var sum = require('../src/sum.js');
+const sum = require('../src/sum.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('adds -1 + 2 to equal 1', () => {
+  expect(sum(-1, 2)).toBe(1);
+});
+
+test('adds -9 + 0 to equal -9', () => {
+  expect(sum(-9, 0)).toBe(-9);
+});
+
+test('adds -9 + -8 to equal -17', () => {
+  expect(sum(-9, -8)).toBe(-17);
 });
